3be5631670eefb37fd8a1dcfe579c7e37159eacf
Lucile Garrigoux <lgarrigoux@hotmail.com>
2022-02-10 17:02:22 +0100

    Debugging branch deployment.

--- scripts/build_and_publish_docker_image.sh
+++ scripts/build_and_publish_docker_image.sh
@@ -4,9 +4,11 @@ set -e
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
     TAG=$CIRCLE_BUILD_NUM;
-if [ "$CIRCLE_BRANCH" != "master" ]; then
+elif [ "$CIRCLE_BRANCH" != "master" ]; then
         TAG="latest"
 
-fidocker login --username $DOCKER_USER --password $DOCKER_PASS
-docker build -t shardlabs/app:$TAG .
-docker push shardlabs/app:$TAG
+fi
+
+docker login --username $DOCKER_USER --password $DOCKER_PASS
+docker build -t lgarrigoux/oc_lettings:$TAG .
+docker push lgarrigoux/oc_lettings:$TAG
