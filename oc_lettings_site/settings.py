cb2cd6442c57c4438820acebc43c6b86fcee29b3
Lucile Garrigoux <lgarrigoux@hotmail.com>
2022-02-25 12:41:31 +0100

    Attempting another Sentry fix.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -122,12 +122,9 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # SENTRY INTEGRATION
-sentry_sdk.init(
-    dsn=config('SENTRY_DNS', default='theSecretestKey'),
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-)
+if DEBUG == True:
+    sentry_sdk.init(
+        dsn=config('SENTRY_DSN', default="secretkey"),
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+    )
